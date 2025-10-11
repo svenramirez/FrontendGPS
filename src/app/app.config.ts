9231724db3db0245e7b269ac76043864d60fb75e
code3743 <jota.lopez@correounivalle.edu.co>
2025-10-10 20:20:02 -0500

    refactor: remove styleUrl from AppComponent and update HttpClient configuration in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,17 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { authInterceptor} from './core/interceptor/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([ authInterceptor])),
     importProvidersFrom(FormsModule, CommonModule)
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  //{ path: 'register', component: RegisterComponent }
 ];
